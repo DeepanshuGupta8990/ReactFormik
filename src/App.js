5e05e96dda1c8165432b072bf099e495b189fa5a
Deepanshu Gupta <DEEPANSHUGUPTA899@GMAIL.COM>
2023-07-31 19:25:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
 import './App.css';
+import NewYoutubeform from './components/NewYoutubeForm';
 import Youtubeform from './components/Youtubeform';
+import NewYoutubeform2 from './components/newYoutubeForm2';
 
 function App() {
   return (
     <div className="App">
-      <Youtubeform/>
+      {/* <Youtubeform/> */}
+      {/* <NewYoutubeform/> */}
+      <NewYoutubeform2/>
     </div>
   );
 }
