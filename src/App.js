249bbe6facec65e43930d4df7da4a08c54b739aa
Deepanshu Gupta <DEEPANSHUGUPTA899@GMAIL.COM>
2023-08-04 14:18:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import NewYoutubeform from './components/NewYoutubeForm';
 import Youtubeform from './components/Youtubeform';
 import FormWithManuallyTriggeringValidation from './components/FormWithManuallyTriggeringValidation';
 import NewYoutubeform2 from './components/newYoutubeForm2';
+import DisablingButton from './components/DisablingButton';
+import { styled } from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       {/* <Youtubeform/> */}
       {/* <NewYoutubeform/> */}
-      {/* <NewYoutubeform2/> */}
+      <NewYoutubeform2/>
      <FormWithManuallyTriggeringValidation/>
-    </div>
+     <DisablingButton/>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+/* align-items: center; */
+flex-wrap: wrap;
+gap: 1rem;
+background-color: #e6a4a4;
+`
